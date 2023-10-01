f2803b420e71ebae3770b5cc716181128c628a90
John Angelo S. Soriano <sorianojohn008@gmail.com>
2023-10-01 20:26:38 +0800

    Added new route for posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,3 +3,23 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 // Define routes
+router.get('/', (req, res) => {
+    res.render('post/post', {
+        subreddits: [
+
+            // These are test data to simulate dynamics
+            {
+                heading: "Taylor Swift and Travis Scott",
+                subheading: "Taylor Swift and Travis Scott are dating!",
+                subreddit : "/fauxmau",
+            },
+            {
+                heading: "F1 2021 Season won't push through",
+                subheading: "F1 2021 Season won't push through due to COVID-19",
+                subreddit : "/formula1",
+            }
+        ],
+    });
+});
+
+module.exports = router;
