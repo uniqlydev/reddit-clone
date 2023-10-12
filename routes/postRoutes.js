f76c880ac0a5ad3a6972fe3455cabfbfb1965d85
John Angelo S. Soriano <sorianojohn008@gmail.com>
2023-10-12 19:22:43 +0800

    Partially implemented nested comments

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,7 +6,7 @@ const postController = require('../controllers/postController');
 router.get('/', (req, res) => {
     res.render('post/post', {
         post_details: {
-            subreddit: "r/nba",
+            subreddit: "nba",
             post_id: 1,
             title: "[Wojnarowski] BREAKING: The Portland Trail Blazers are trading guard Damian Lillard to the Milwaukee Bucks, sources tell ESPN. ",
             content: {
@@ -15,32 +15,54 @@ router.get('/', (req, res) => {
             },
             date_posted: "2023-10-11",
             author: "CCAPDEV-sample",
-            votes: 25000
+            votes: 2500
         },
         comments: [
             // These are test data to simulate dynamics
-            {
-                id: 1,
-                // reply_id: null, // reply id for phase 2
-                content: {
-                    commenter: "Number1GiannisFan",
-                    created_date: "2023-10-11",
-                    votes:25,
-                    text: "As a fan of both, I'm excited to see this pairing!"
+                {
+                    id: 1,
+                    // reply_id: null, // reply id for phase 2
+                    content: {
+                        commenter: "Number1GiannisFan",
+                        created_date: "2023-10-11",
+                        votes:25,
+                        text: "As a fan of both, I'm excited to see this pairing!"
+                    },
+                    children: [
+                        {
+                            id: 2,
+                            content: {
+                                commenter: "Number2GiannisFan",
+                                created_date: "2023-10-11",
+                                votes:12,
+                                text: "I don't even need to watch, just give them the trophy already!"
+                            },
+                            children: [
+                                {
+                                    id: 2,
+                                    content: {
+                                        commenter: "Number3DameFan",
+                                        created_date: "2023-10-11",
+                                        votes:12,
+                                        text: "fdsfgd"
+                                    },
+                                    children: []
+                                }
+                            ]
+                        },
+                    ]
                 },
-                children_id: [2]
-            },
-            {
-                id: 2,
-                // reply_id: 1,
-                content: {
-                    commenter: "Number2GiannisFan",
-                    created_date: "2023-10-11",
-                    votes:12,
-                    text: "I don't even need to watch, just give them the trophy already!"
-                },
-                children_id: []
-            }
+            // {
+            //     id: 2,
+            //     // reply_id: 1,
+            //     content: {
+            //         commenter: "Number2GiannisFan",
+            //         created_date: "2023-10-11",
+            //         votes:12,
+            //         text: "I don't even need to watch, just give them the trophy already!"
+            //     },
+            //     children_id: []
+            // }
         ],
     });
 });
