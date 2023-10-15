9e89407e376a47c923f6fcb25ca1f229327cac8f
Matthew Wassmer <110731303+Maccheww@users.noreply.github.com>
2023-10-15 22:43:09 +0800

    Added login/register page
    Adjust login and register pages to fit with current Phase 1 specs. Includes basic functionality for both login and register

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,15 +1,15 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-    const login = document.getElementById('loginbtn');
+//     const login = document.getElementById('loginbtn');
 
-    login.addEventListener('click', () => {
-        const username = prompt("Please enter your username");
-        window.location.href = `/profile?username=${username}`;
-    });
+//     login.addEventListener('click', () => {
+//         const username = prompt("Please enter your username");
+//         window.location.href = `/profile?username=${username}`;
+//     });
 
 
 
     
 
-});
+// });
