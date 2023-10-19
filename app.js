25141a185a74ed1d6d570750ed87bf5db034d370
Matthew Wassmer <110731303+Maccheww@users.noreply.github.com>
2023-10-19 14:40:30 +0800

    Added comments for profileEdit to be fixed

--- app.js
+++ app.js
@@ -234,11 +234,13 @@ app.get('/profile', (req, res) => {
 
 // Profile Edit page
 app.get('/profile-edit', (req, res) => {
+    // Bren, I couldn't get this to work. Please check if free.
+
+    const username = req.query.username;
+    const user = getUserDataByUsername(username);
+
     res.render('home/profileEdit', {
-        profile: [
-            username = "TechEnthusiast21",
-            bio = "Passionate about all things tech!",
-        ],
+        profile: user,
     });
 });
 
