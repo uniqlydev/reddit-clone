317d863f6cb6f2cf822296fb50f7224837007203
Matthew Wassmer <110731303+Maccheww@users.noreply.github.com>
2023-11-02 02:18:07 +0800

    Removed unnecessary information from profile page for user data

--- models/user.js
+++ models/user.js
@@ -2,15 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    id: String,
     username: String,
     password: String,
     bio: String,
-    memberSince: String,
-    bday: String,
     memberURL: String,
     avatar: String,
-    karma: Number,
 });
 
 const user = mongoose.model('user', userSchema);
